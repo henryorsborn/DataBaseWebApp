1b9dae786fb86dc93cf2d0c5f214fac2d17a910f
henryorsborn <50592835+henryorsborn@users.noreply.github.com>
2019-11-11 13:30:12 -0500

    created VERY basic setup

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewort" content="width=device-width, initial scale=1">
+		<title>Database Manager</title>
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrap/3.3.5/css/bootstrap.min.css">
+
+	</head>
+	<body>
+		<h1>These the tables we found</h1>	
+		
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
+
